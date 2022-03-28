5f81c1076e6387d6feb72bcb968472c3401154d9
Mihajlo Mitić <mihajlomiticmiki@gmail.com>
2022-03-28 14:49:54 +0200

    feat: feign client connection for vehicleServiceApi

--- src/main/java/levi9/UserService/Controller.java
+++ src/main/java/levi9/UserService/Controller.java
@@ -2,14 +2,23 @@ package levi9.UserService;
 
 import levi9.UserService.api.Example1;
 import levi9.UserService.api.UserServiceApi;
+import levi9.VehicleService.api.VehicleServiceApi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller{
+    @Autowired
+    private VehicleServiceApi vehicleServiceApi;
 
     @GetMapping(value = "/")
     public Example1 example() {
         return new Example1("aaaaa");
+    }
+
+    @GetMapping("/vehicle")
+    public String example1(){
+        return vehicleServiceApi.example1();
     }
 }
--- src/main/java/levi9/UserService/UserServiceApplication.java
+++ src/main/java/levi9/UserService/UserServiceApplication.java
@@ -1,12 +1,13 @@
 package levi9.UserService;
 
+import levi9.VehicleService.api.VehicleServiceApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
-@EnableFeignClients
+@EnableFeignClients(clients = VehicleServiceApi.class)
 @SpringBootApplication
 public class UserServiceApplication {
 
