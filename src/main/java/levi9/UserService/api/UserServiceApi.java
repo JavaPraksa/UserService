56b1a257628e14fbaf8f73695ad02a9ca7427cf5
dejandeki123 <dejanpetrusic123@gmail.com>
2022-04-12 10:45:31 +0200

    Update UserServiceApi.java
    deleted unnecessary things

--- src/main/java/levi9/UserService/api/UserServiceApi.java
+++ src/main/java/levi9/UserService/api/UserServiceApi.java
@@ -1,11 +1,12 @@
 package levi9.UserService.api;
 
-import levi9.UserService.dto.UserDto;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 @FeignClient(name = "user-service")
 public interface UserServiceApi {
@@ -14,9 +15,6 @@ public interface UserServiceApi {
 
     @GetMapping(value = "/auth/is-authenticated")
     Boolean isAuthenticated(@RequestParam String token);
-
-    @GetMapping(value = "/user/{username}")
-    ResponseEntity<UserDto> getUserByUsername(@PathVariable String username);
 
     @GetMapping("/user/getEmailById/{id}")
     String getUserEmailById(@PathVariable Long id);
